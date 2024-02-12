73b829a64762ff9251060be4854d5b1d334d0cf2
nadiyamolkm <nadiyamolkm@gmail.com>
2024-02-12 18:47:56 +0530

    service not needed

--- Services/bookServices.js
+++
@@ -1,8 +0,0 @@
-const connection = require('../dbConnection')
-
-const getAllBooks= async()=>{
-    const result = await connection.query("SELECT * FROM bookstore")
-    .catch((err)=>{
-        console.log("Error in fetching books", err)
-    })
-}
